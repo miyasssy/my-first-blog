027b4b9bd98568cf634d4539abd33124effc968a
h.miyashita <pastello41@gmail.com>
2020-10-11 22:18:44 +0300

    changes in post_models

--- blog/models.py
+++ blog/models.py
@@ -6,9 +6,31 @@ from django.utils import timezone
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
+    journal = models.CharField(max_length=200,
+    choices = [
+    ("NEJM","NEJM"),("JACC","JACC"),("Circulation","Circulation"),
+    ],
+    null=True
+    )
+    published_year = models.CharField(
+    max_length = 50,
+    choices = [("2016","2016年"),("2017","2017年"),("2018","2018年"),
+    ("2019","2019年"),("2020","2020年"),
+    ],
+    null=True
+    )
     text = models.TextField()
+    category = models.CharField(
+    max_length = 50,
+    choices = [
+    ("TAVR","TAVR"),("CAD","CAD"),("Economic","Economic"),
+    ],
+    null=True
+    )
+    Tag = models.CharField(max_length = 50,blank=True)
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
+
 
     def publish(self):
         self.published_date = timezone.now()
